1301ff94d0552d4f86040a1a198d2c32f27a931c
Chris Mackey <chris@ladybug.tools>
2022-08-12 11:23:04 -0400

    feat(visualization): Refactor GraphicContainer into VisualizationSet
    This commit also includes the addition of a layer specification to ladybug-display objects.

---
+++ tests/test_visualization.py
@@ -0,0 +1,11 @@
+import os
+from ladybug_display_schema.visualization import VisualizationSet
+
+# target folder where all of the samples live
+root = os.path.dirname(os.path.dirname(__file__))
+target_folder = os.path.join(root, 'samples')
+
+
+def test_visualization_set():
+    file_path = os.path.join(target_folder, 'visualization.json')
+    VisualizationSet.parse_file(file_path)
