0024569976d04fa0fed6fb1e8bb1d4917ec86079
Chris Mackey <chris@ladybug.tools>
2022-10-12 15:52:16 -0400

    feat(visualization): Refactor VisualizationSet to align with VTK
    This includes the following:
    
    * Create a ContextGeometry object (similar to the AnalysisGeometry object)
    * Add display_name and identifier to AnalysisGeometry and ContextGeometry. This display_name will determine the layer onto which the geometry objects are written. We will use the Rhino convention for specifying sub-layers by separating the parent::child with ::
    * Instead of having separate properties on the VisualizationSet for context_geometry and analysis_geometry, just make one "geometry" property that contains a flat list of context and analysis geometries. This will allow people to customize the order of layers and alternate between context and analysis geometries.
    * Change the display_mode enumeration to not have "Shaded" (this has no meaning for VTK or Rhino) and instead have "Points" (which has meaning for VTK and Rhino)
    * Add a display_mode option to AnalysisGeometry, which will allow users to choose whether they want the meshes to display with edges or not.

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -9,3 +9,8 @@ target_folder = os.path.join(root, 'samples')
 def test_visualization_set():
     file_path = os.path.join(target_folder, 'visualization.json')
     VisualizationSet.parse_file(file_path)
+
+
+def test_visualization_set_detailed():
+    file_path = os.path.join(target_folder, 'visualization_detailed.json')
+    VisualizationSet.parse_file(file_path)
