3deb365c62bcacf053e7f8739f9f55fbbd1d41d8
Chris Mackey <chris@ladybug.tools>
2025-09-02 14:40:31 -0700

    ci(release): Upgrade all dependencies used for release

--- setup.py
+++ setup.py
@@ -21,7 +21,11 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
         "Operating System :: OS Independent"
     ],
